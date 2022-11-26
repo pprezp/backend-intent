5a96f13e8bc043fbf40658362e50bd597553e48e
Pablo Perez <pablo.prezparedes@outlook.com>
2022-11-26 12:47:15 -0600

    update get to /

--- routes/root/Root.js
+++ routes/root/Root.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 module.exports = () => {
     router.get('/', (req, res) => {
-        res.status(200).send("Has entrado al root del backend")
+        res.status(200).send("Has entrado al root del backend, ¡Bienvenido!")
     });
 
     return router;
